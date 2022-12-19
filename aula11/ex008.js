132e5247d1ad95d05bac2f0c3baee7adebdff54f
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-19 13:48:14 +0000

    ex08 em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+let velNorm = 120
+let velMax = 120
+let velMid = 90
+let velMin = 60
+let multaMax = 1550
+let multaMid = 600
+console.log(`Você está indo a uma velocidade de ${velNorm}Km/h.`)
+if(velNorm > velMax){
+    console.log(`A velocidade máxima permitida nessa estrada é ${velMax}Km/h , você foi multado em  ${multaMax.toLocaleString('pt-BR', {style : 'currency', currency : 'BRL'})} reais.`)
+}else if(velNorm > velMid){
+    console.log(`A velocidade limite nessa estrada é de ${velMid}Km/h. Você foi multado em ${multaMid.toLocaleString('pt-BR', {style : 'currency', currency :'BRL'})} reais.`)
+}else{
+    console.log('Muito bem. Use cinto de segurança. Dirija com cuidado!!')
+}
