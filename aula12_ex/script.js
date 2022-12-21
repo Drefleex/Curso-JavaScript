51cf758163c3dbd361dfaff5a971803cc554dfb5
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-21 15:21:50 +0000

    Update
    update

--- aula12_ex/script.js
+++ aula12_ex/script.js
@@ -3,19 +3,20 @@
     var msg1 = document.querySelector('p#pdia')
     var img = document.getElementById('imagem')
     var agora = new Date()
-    var horas =agora.getHours()
+   // var horas =agora.getHours()
+   var horas = 5
 
     msg.innerHTML = `Agora são ${horas} horas`
     if(horas >= 5 && horas < 12){
-         msg1.innerHTML = 'Bom Dia'
+         msg1.innerHTML = 'Tenha um ótimo Dia'
          img.src = 'imagens/dia.png'
     }else if(horas >= 12 && horas <= 18){
          img.src = 'imagens/tarde.png'
-         msg1.innerHTML = 'Boa tarde'
+         msg1.innerHTML = 'Tenha uma ótima tarde'
          document.body.style.backgroundColor = '#DBAE17'
    } else {
          img.src = 'imagens/noite.png'
-         msg1.innerHTML = 'Boa Noite'
+         msg1.innerHTML = 'Tenha uma ótima noite'
          document.body.style.backgroundColor = '#05152E'
    }
  }
