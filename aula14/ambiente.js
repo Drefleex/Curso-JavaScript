fff5f67ef73e45a1be954cb60633cd589b2191df
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-25 20:16:39 +0000

    Update
    Update

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+
+/*
+var c = 0
+while( c < 6){
+    console.log('Olá')
+    c++} 
+*/
+console.log('---------- Inicio --------')
+for( var c = 0; c <= 100 ; c += 10){
+    console.log(c)
+}
+console.log('---------- Fim --------')
