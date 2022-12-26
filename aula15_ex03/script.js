e14042e719e4259da979f61dbcd66f7a383b287a
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-26 00:40:50 +0000

    update
    update

--- aula15_ex03/script.js
+++ aula15_ex03/script.js
@@ -1,23 +1,38 @@
 function contar(){
-    var cont = document.querySelector('input#cont')
+    var ini = document.querySelector('input#cont')
     var end = document.querySelector('input#end')
     var jum = document.querySelector('input#jum')
     var res = document.querySelector('div#res')
 
-    var c = Number(cont.value)
-    var e = Number(end.value)
-    var j = Number(jum.value)
     
-    if( c  === 0 || e === 0 ){
+    // Verificador
+    if( ini.value.length  == 0 || end.value.length == 0 || jum.value.length == 0){
         window.alert(`Impossível contar, Digite um valor válido!!`)
-    } else if ( j === 0){
-        window.alert(`Passo Inválido , Considerando PASSO 1`)
-        j += 1
+    }else{
+        res.innerHTML = `Contando: <br> `
+        var i = Number(ini.value)
+        var e = Number(end.value)
+        var j = Number(jum.value)
+        if ( j <= 0){
+            window.alert(`Passo Inválido , Considerando PASSO 1`)
+            j = 1
+        }
+        //Logica contador
+    if( i < e) {
+        
+        for(let c = i ; c < e ; c += j){
+            console.log(c)
+            res.innerText += `${c} \u{1F449}`
+        }
+      
+    }else{
+        for(let c = i ;  c >= e ; c -= j){
+            res.innerText += `${c} \u{1F449}`
+        }
+        
     }
+    res.innerHTML += `\u{1F3C1}`
 
-    for(c ; c < e ; c += j){
-        console.log(c)
-    }
+  }
     
-    res.innerHTML =`Contando: ${c} - `
 }
