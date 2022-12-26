9f69c78b30c87801be7868ec9864dbbac5f0131f
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-26 12:50:59 +0000

    Update
    update

--- aula15_ex03/script.js
+++ aula15_ex03/script.js
@@ -1,38 +1,33 @@
 function contar(){
-    var ini = document.querySelector('input#cont')
+    var ini = document.querySelector('input#ini')
     var end = document.querySelector('input#end')
-    var jum = document.querySelector('input#jum')
+    var pul = document.querySelector('input#pul')
     var res = document.querySelector('div#res')
 
-    
-    // Verificador
-    if( ini.value.length  == 0 || end.value.length == 0 || jum.value.length == 0){
-        window.alert(`Impossível contar, Digite um valor válido!!`)
-    }else{
-        res.innerHTML = `Contando: <br> `
+    if(ini.value.length == 0 || end.value.length == 0){ 
+            window.alert(`Dados inválidos \u{274C}`)
+            
+    } else {
+        res.innerHTML = `Contando: `
         var i = Number(ini.value)
         var e = Number(end.value)
-        var j = Number(jum.value)
-        if ( j <= 0){
-            window.alert(`Passo Inválido , Considerando PASSO 1`)
-            j = 1
+        var p = Number(pul.value)
+        if (p <= 0 ){
+            window.alert(`Valor inválido. PASSO = 1`)
+            p += 1
         }
-        //Logica contador
-    if( i < e) {
-        
-        for(let c = i ; c < e ; c += j){
-            console.log(c)
-            res.innerText += `${c} \u{1F449}`
-        }
-      
-    }else{
-        for(let c = i ;  c >= e ; c -= j){
-            res.innerText += `${c} \u{1F449}`
-        }
-        
-    }
-    res.innerHTML += `\u{1F3C1}`
+      if(i < e){
+            for( let c = i ; c < e ; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i ; c > e ; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
 
-  }
-    
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML += `\u{1F6A9}`
+    }
+   
 }
