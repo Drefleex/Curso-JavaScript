c4085828ca4552b5873dade19da696b0b6000cde
André Felipe <95745499+Drefleex@users.noreply.github.com>
2022-12-28 15:25:03 +0000

    update
    update

--- aula16_ex05/script.js
+++ aula16_ex05/script.js
@@ -1,62 +1,67 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flist')
-let res = document.querySelector('div#res')
+let res =   document.querySelector('div#res')
 let valores = []
 
-function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
-        return true
-    } else {
-        return false
-    }
+function isNumber(n){
+  if(Number(n) >= 1 && Number(n) <= 100){
+    return true
+  } else {
+    return false
+  }
 }
 
-function inLista(n, l){
-      if(l.indexOf(Number(n)) != -1){
-         return true
-      } else {
-        return false
-      }
+function inList(n,l){
+  if(l.indexOf(Number(n)) != -1){
+    return true
+  } else {
+    return false
+  }
 }
 
 function adicionar(){
-      if(isNumero(num.value) && !inLista(num.value, valores)){
-                valores.push(Number(num.value))
-                let item = document.createElement('option')
-                item.text =` Valor ${num.value} adicionado`
-                lista.appendChild(item)
-                res.innerHTML = ''
-      } else {
-        window.alert(`Valor inválido ou já encontrado na lista.`)
-      }
-      num.value = ''
-      num.focus()
+  if(isNumber(num.value) && !inList(num.value, valores)){
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text =`O valor foi ${num.value} adicionado.`
+    lista.appendChild(item)
+    res.innerHTML = ''
+  } else {
+    window.alert('Dados inválidos ou já existente.')
+  }
+  num.value = ''
+  num.push()
 }
 
 function finalizar(){
-  if( valores.length == 0){
-    window.alert(`Adicione valores para finalizar.`)
+  if(valores.length == 0){
+    window.alert('Nenhum valor encontrado, tente novamente!')
   } else {
-      let tot = valores.length
-      let maior = valores[0]
-      let menor = valores[0]
-      let soma = 0
-      let media = 0
-      for(let pos in valores){
-         soma += valores[pos]
-        if(valores[pos] > maior){
-          maior = valores[pos]
-        }
-        if (valores[pos] < menor){
-          menor = valores[pos]
-        }
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+    for(let p in valores){
+      soma += valores[p]
+      if(valores[p] > maior){
+        maior = valores[p]
       }
-      media = soma / tot
-      res.innerHTML = ''
-      res.innerHTML += `<p>Ao todo temos ${tot} número(s) cadastrados </p>`
-      res.innerHTML += `<p>O maior valor  informado foi ${maior}</p>`
-      res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
-      res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
-      res.innerHTML += `<p>A media dos valores digitados é ${parseFloat(media)}</p>`
+      if(valores[p] < menor){
+        menor = valores[p]
+      }
+    }
+    media = soma / tot
+    res.innerHTML = ''
+    res.style.textAlign = 'center'
+    res.style.padding = '8px'
+    res.innerHTML += `<p>O sistema possui ${tot} dados cadastrados no sistema!</p>`
+    res.innerHTML += `<p>O maior valor do sistema é ${maior}</p>`
+    res.innerHTML += `<p>O menor valor do sistema é ${menor}</p>`
+    res.innerHTML += `<p>Totalizando todos os valores do sistema:${soma}`
+    res.innerHTML += `<p>A média dos valores inseridos é ${media}`
+   
   }
 }
+
+  
